cacff4725027f6da47605b404225d8270e319b41
daniel <danielfischbach90@gmail.com>
2021-09-10 16:05:26 +0200

    - success

--- app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -17,12 +17,14 @@ package com.example.android.explicitintent;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO (1) Retrieve the text from the EditText and store it in a variable
-
+                String textEntered = mNameEntry.getText().toString();
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
                  * just used "this" or "MainActivity.this" in the method call below. However, we
@@ -74,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
                 // TODO (2) Use the putExtra method to put the String from the EditText in the Intent
-
+                startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
                 /*
                  * Once the Intent has been created, we can use Activity's method, "startActivity"
                  * to start the ChildActivity.
