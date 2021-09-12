87f430e55ed21fbb346f13cd067346f49885fc3b
daniel <danielfischbach90@gmail.com>
2021-09-12 11:14:23 +0200

    - solved

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,10 +15,13 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -96,16 +99,24 @@ public class MainActivity extends AppCompatActivity {
          * to view particular content. In this case, our webpage URL.
          */
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-
+        Log.w("Intent info", url);
         // COMPLETED (4) Verify that this Intent can be launched and then call startActivity
         /*
          * This is a check we perform with every implicit Intent that we launch. In some cases,
          * the device where this code is running might not have an Activity to perform the action
          * with the data we've specified. Without this check, in those cases your app would crash.
          */
-        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        //intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        /*if  (intent.resolveActivity(getPackageManager()) == null){
+            Log.w("Intent info", "resolve Activity failed");
+        }
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        }*/
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
